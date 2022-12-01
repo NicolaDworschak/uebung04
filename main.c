f2156624487273f05188261daae28bff89c40bd9
Nicola Dworschak <nicola.dworschak.1@gmail.com>
2022-12-01 10:17:34 +0100

    update dreieck

--- main.c
+++ main.c
@@ -18,8 +18,8 @@ void dreieck(int anzahl_zeilen) {
     // print out triangle with 'X'
     char arr[1000];
     int index=0;
-    for (int i=0; i<anzahl_zeilen; i++) {
-        for (int j=0; j<i; j++) {
+    for (int i=1; i<=anzahl_zeilen; i++) {
+        for (int j=1; j<=i; j++) {
             arr[index] = 'X';
             index++;
         }
